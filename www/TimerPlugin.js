7afadd428a99af8793759b2d8cb588a85ebf1742
christian.schneider <christian.schneider@coredinate.de>
2015-10-28 14:03:34 +0100

    refactoring

--- www/TimerPlugin.js
+++ www/TimerPlugin.js
@@ -12,34 +12,34 @@ var callbacks = {};
 
 var TimerPlugin = {
     addInterval: function(msInterval, callback, success, error){
-        cordova.exec(function(id){
+        exec(function(id){
             callbacks[id] = callback;
             if(typeof success !== "undefined")
                 success(id);
         }, function(err){
             if(typeof err !== "undefined")
                 error(err)
-        }, "TimerPlugin", "addInterval", msInterval, callback);
+        }, "TimerPlugin", "addInterval", [msInterval]);
     },
     addTimeout: function(msTimeout, callback){
-        cordova.exec(function(id){
+        exec(function(id){
             callbacks[id] = callback;
             if(typeof success !== "undefined")
                 success(id);
         }, function(err){
             if(typeof err !== "undefined")
                 error(err)
-        }, "TimerPlugin", "addTimeout", msTimeout, callback);
+        }, "TimerPlugin", "addTimeout", [msTimeout]);
     },
     deleteTimer: function(timerId){
-        cordova.exec(function(res){
+        exec(function(res){
             delete callbacks[id];
             if(typeof success !== "undefined")
                 success(res);
         }, function(err){
             if(typeof err !== "undefined")
                 error(err)
-        }, "TimerPlugin", "deleteTimer", timerId);
+        }, "TimerPlugin", "deleteTimer", [timerId]);
     },
     triggerTimer: function(timerId){
         console.log("timer triggered: " + timerId);

41fa071bcd5f05f7cebaf06e808d18da0f9532c4
christian.schneider <christian.schneider@coredinate.de>
2015-10-28 15:18:05 +0100

    added ionic wrapper

---
+++ ionic/TimerPlugin.js
@@ -0,0 +1,53 @@
+/**
+ * Created by christian.schneider on 28.10.2015.
+ */
+app.service("TimerPlugin", function(){
+
+    var self = this;
+
+    var canBeUsed = false;
+    var engine = null;
+
+    if(typeof cordova.plugins.TimerPlugin !== "undefined"){
+
+        engine = cordova.plugins.TimerPlugin;
+        canBeUsed = true;
+
+        console.log("TimerPlugin loaded");
+
+    }
+
+    var notUsable = function(){
+        console.log("TimerPlugin not loaded");
+    };
+
+    this.timeout = function(callback, duration){
+
+        if(canBeUsed)
+            return cordova.plugins.TimerPlugin.addTimeout(duration, callback);
+        else
+            notUsable();
+
+    };
+
+    this.interval = function(callback, duration){
+
+        if(canBeUsed)
+            return cordova.plugins.TimerPlugin.addInterval(duration, callback);
+        else
+            notUsable();
+
+    };
+
+    this.cancel = function(id){
+
+        if(canBeUsed)
+            return cordova.plugins.TimerPlugin.deleteTimer(id);
+        else
+            notUsable();
+
+    };
+
+    return this;
+
+});

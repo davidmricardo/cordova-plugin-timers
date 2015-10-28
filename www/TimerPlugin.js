7a7ac9d4d768e034546622d228e1e9c4607b3472
christian.schneider <christian.schneider@coredinate.de>
2015-10-28 13:30:59 +0100

    refactoring

--- www/TimerPlugin.js
+++ www/TimerPlugin.js
@@ -12,7 +12,7 @@ var callbacks = {};
 
 var TimerPlugin = {
     addInterval: function(msInterval, callback, success, error){
-        exec(function(id){
+        cordova.exec(function(id){
             callbacks[id] = callback;
             if(typeof success !== "undefined")
                 success(id);
@@ -22,17 +22,17 @@ var TimerPlugin = {
         }, "TimerPlugin", "addInterval", msInterval, callback);
     },
     addTimeout: function(msTimeout, callback){
-        exec(function(id){
+        cordova.exec(function(id){
             callbacks[id] = callback;
             if(typeof success !== "undefined")
                 success(id);
-        }, function(errr){
+        }, function(error){
             if(typeof error !== "undefined")
                 error(err)
         }, "TimerPlugin", "addTimeout", msTimeout, callback);
     },
     deleteTimer: function(timerId){
-        exec(function(res){
+        cordova.exec(function(res){
             delete callbacks[id];
             if(typeof success !== "undefined")
                 success(res);

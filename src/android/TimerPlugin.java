2b42050b3064ed41e292e63cfa68af300020e939
christian.schneider <christian.schneider@coredinate.de>
2015-10-29 13:05:07 +0100

    working

--- src/android/TimerPlugin.java
+++ src/android/TimerPlugin.java
@@ -8,6 +8,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 public class TimerPlugin extends CordovaPlugin {
@@ -24,6 +30,8 @@ public class TimerPlugin extends CordovaPlugin {
 		TimerPlugin.webView = super.webView;
 		TimerPlugin.cordovaInstance = super.cordova;
 		Log.v(TAG, "init");
+		RegisterAlarmBroadcast();
+		alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10000 , pendingIntent); 
 	}
 
 	@Override
@@ -37,12 +45,43 @@ public class TimerPlugin extends CordovaPlugin {
 			callbackContext.success(addInterval(data.optInt(0)));
 		else if (action.equals("deleteTimer"))
 			callbackContext.success(deleteTimer(data.optInt(0)));
+		else if (action.equals("dummy"))
+			callbackContext.success(1);
 		else
 			return false;
 
 		return true;
 
 	}
+	
+	AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    BroadcastReceiver mReceiver;
+	
+ 	private void RegisterAlarmBroadcast() {
+ 		
+		mReceiver = new BroadcastReceiver() {
+			    // private static final String TAG = "Alarm Example Receiver";
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				Log.v(TAG, "tick");
+			    //Toast.makeText(context, "Alarm time has been reached", Toast.LENGTH_LONG).show();
+			    }
+	
+		};
+		
+		Activity a = cordovaInstance.getActivity();
+		a.registerReceiver(mReceiver, new IntentFilter("sample"));
+		pendingIntent = PendingIntent.getBroadcast(a, 0, new Intent("sample"), 0);
+		    alarmManager = (AlarmManager)(a.getSystemService(Context.ALARM_SERVICE));
+		    
+	}
+		
+	/*private void UnregisterAlarmBroadcast() {
+		Activity a = cordovaInstance.getActivity();
+	    alarmManager.cancel(pendingIntent); 
+	    a.getBaseContext().unregisterReceiver(mReceiver);
+	}*/
 
 	private int addInterval(int msInterval) {
 		Log.v(TAG, "addInterval(" + msInterval + ")");
